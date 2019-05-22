98121de38b9402d8ec2a0a805047d461b52af15d
Rrezarta Sallauka <rrezarta.sallauka@studentet.uni-pr.edu>
2019-05-22 17:51:10 +0200

    main page

--- MainPage.php
+++ MainPage.php
@@ -1,4 +1,170 @@
 			<?php include('header.php');?>
+			<link rel="stylesheet" type="text/css" href="css/MainPage.css">
+
+			<main>
+
+			    <div class="container">
+
+			        <div class="main-top">
+			            <nav>
+			                <ul>
+			                    <li>
+			                        <a href="mainproject.htm">
+			                            HOME
+			                        </a>
+			                    </li>
+			                    <li>
+			                        <a href="about.html">ABOUT</a>
+			                    </li>
+			                    <li>
+			                        <a href="company.html">COMPANY</a>
+			                    </li>
+			                    <li>
+			                        <a href="services.html">SERVICES</a>
+			                    </li>
+			                    <li class="dropdown">
+			                        <a href="products.html">
+			                            PRODUCTS
+			                        </a>
+			                        <ul class="dropdown_menu">
+			                            <li>
+			                                <a href="phones.html">Phones</a>
+			                            </li>
+			                            <li>
+			                                <a href="https://swappa.com/laptops">Laptops</a>
+			                            </li>
+			                            <li>
+			                                <a href="https://swappa.com/cameras">Cameras</a>
+			                            </li>
+			                            <li>
+			                                <a href="games.html">Games</a>
+			                            </li>
+			                            <li>
+			                                <a href="https://swappa.com/buy/tablets">Tablets</a>
+			                            </li>
+			                            <li>
+			                                <a href="https://swappa.com/wearables">Watches</a>
+			                            </li>
+			                        </ul>
+			                    </li>
+			                    <li>
+			                        <a href="gallery.html">GALLERY</a>
+			                    </li>
+			                    <li>
+			                        <a href="protfolio.html">PORTFOLIO</a>
+			                    </li>
+			                    <li class="dropdown">
+			                        <a href="introduction.html">
+			                            WHY US
+			                        </a>
+			                        <ul class="dropdown_menu">
+			                            <li>
+			                                <a href="introduction.html">Introduction</a>
+			                            </li>
+			                            <li>
+			                                <a href="customers.html">Costumers</a>
+			                            </li>
+			                            <li>
+			                                <a href="security.html">Security</a>
+			                            </li>
+			                            <li>
+			                                <a href="special-offer.html">Special Offer</a>
+			                            </li>
+			                            <li>
+			                                <a href="news.html">News and Information</a>
+			                            </li>
+			                            <li>
+			                                <a href="partners.html">Partners</a>
+			                            </li>
+			                        </ul>
+			                    </li>
+			                </ul>
+
+			            </nav>
+			        </div>
+			        <!-- ==================================2========================================= -->
+
+			        <!-- **********************************3***************************************** -->
+			        <div class="maintop1">
+
+			            <div class="maintop2">
+			                <h1 id="logo" title="Exoy">Exoy</h1>
+			                <p>
+			                    <u>Tech Marketplace</u>
+			                </p>
+			            </div>
+
+			            <div class="maintop3">
+			                <input class="input1" type="text" placeholder="Search Our Website" title="Search our website"/>
+			                <input class="input2" type="submit" value="SEARCH" title="Search">
+			            </div>
+			        </div>
+			    </div>
+			    <section class="about-us-banner small-banner">
+			      <h4>About Us</h4>
+			    </section>
+			    <!--***************************************4********************************************-->
+			    <section class="e1 our-commitment text-center grey-background">
+			        <div class="container">
+			            <h1 class="main-title">Our commitment</h1>
+			            <div class="m1">
+			                <div class="w1 about-description">
+			                    <div class="m2">
+			                        <h3><b>Reinvent credit and investing</b></h3>
+			                        <p class="par">We’re a technology company built on the belief that innovative, creative
+			                            solutions deliver more value and a better experience.</p>
+			                    </div>
+			                    <div class="m2">
+			                        <h3><b>Do what’s right</b></h3>
+			                        <p class="par">We're leading the governance of a new industry, developing ethical, responsible
+			                            ways to bring greater value and better opportunities to customers.</p>
+			                    </div>
+			                </div>
+			                <div class="w2 about-description">
+			                    <div class="m2">
+			                        <h3><b>Deliver world-class experiences</b></h3>
+			                        <p class="par">Accessing credit should be seamless. We’re committed to making borrowing and
+			                            investing simple and easy for everyone.</p>
+			                    </div>
+			                    <div class="m2">
+			                        <h3><b>Level the playing field</b></h3>
+			                        <p class="par">Everyone deserves a better financial future if they strive for it. We’ve built a
+			                            marketplace that keeps costs low and opportunity high.</p>
+			                    </div>
+			                </div>
+			            </div>
+			            <a href="introduction.html" class="btn-element">Read more about us</a>
+			        </div>
+			    </section>
+			    <section class="our-team-section text-center">
+			        <div class="container">
+			            <h1 class="main-title">Meet our team</h1>
+			            <p class="about-us-p">We have an amazing and very creative team</p>
+			            <div class="our-team">
+
+			                <div class="team-member text-center">
+			                    <div class="member-image">
+			                        <img src="Images/puhiza.jpg">
+			                    </div>
+			                    <div class="member-info">
+			                        <h4>Puhiz&#235; Do&#231;i</h4>
+			                        <p class="position">IT Support Technician</p>
+			                    </div>
+			                </div>
+
+			                <div class="team-member text-center">
+			                    <div class="member-image">
+			                        <img src="Images/rrezja.jpg">
+			                    </div>
+			                    <div class="member-info">
+			                        <h4>Rrezarta Sallauka</h4>
+			                        <p class="position">Founder & CEO</p>
+			                    </div>
+			                </div>
+			            </div>
+			        </div>
+			    </section>
+			</main>
 
 
 
