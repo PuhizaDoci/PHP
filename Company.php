c159ab54ca228c6abfa7858467db8a543f37c76c
PuhizaDoci <puhize.doci@studentet.uni-pr.edu>
2019-05-22 15:49:52 +0200

    header ne file

--- Company.php
+++ Company.php
@@ -1,17 +1,5 @@
-<!DOCTYPE html>
-<html manifest="cache.appache">
-	<head>
-		<title>Exoy</title>
-		<script src="js/jquery-1.11.2.min.js"></script>
-		<link href="https://fonts.googleapis.com/css?family=Niramit:300i,400,600&amp;subset=latin-ext,thai,vietnamese" rel="stylesheet">
-		<link href="https://fonts.googleapis.com/css?family=Lora&amp;subset=vietnamese" rel="stylesheet">
-		<link href="https://fonts.googleapis.com/css?family=Sorts+Mill+Goudy" rel="stylesheet">
-		<link href="https://fonts.googleapis.com/css?family=Titillium+Web:300,400,600" rel="stylesheet">
-		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
-		<link rel="stylesheet" type="text/css" href="css/company.css">
-		<link rel="stylesheet" type="text/js" href="js/company.js">
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js">
-		</script>
+<?php include('header.php');?>
+
 		<script>
 		$(document).ready(function(){
 		$("h2").click(function(){
@@ -40,62 +28,6 @@
 
 
 
-
-<header>
-    <div class="container">
-        <div class="header">
-            <div class="header-top">
-                <nav>
-                    <ul>
-                        <li>
-                            <a href="signup.html">Sign Up |</a>
-                        </li>
-                        <li>
-                            <a href="logIn.html">Login |</a>
-                        </li>
-                        <li>
-                            <a href="https://rss.com/">RSS Feeds |</a>
-                        </li>
-                        <li>
-                            <a href="ArchivedNews.html">Archived News</a>
-                        </li>
-                    </ul>
-
-                </nav>
-            </div>
-            <div class="header-bottom">
-                <ul>
-                    <li>
-                        <a href="https://www.linkedin.com" class="linkedIn" target="_blank" title="LinkedIn">
-                            <i class="fab fa-linkedin-in"></i>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="https://twitter.com/" class="twitter" target="_blank" title="Twitter">
-                            <i class="fab fa-twitter"></i>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="https://www.pinterest.com/" class="pinterest" target="_blank" title="Pinterest">
-                            <i class="fab fa-pinterest-p"></i>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="https://plus.google.com" class="googleplus" target="_blank" title="Google Plus">
-                            <i class="fab fa-google-plus-g"></i>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="https://rss.com" class="rss" target="_blank" title="Rss">
-                            <i class="fas fa-rss"></i>
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </div>
-</header>
-<!-- ==================================1========================================= -->
 
 
 <!-- ****************************************2************************************************* -->
---
+++ header.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Exoy</title>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link href="https://fonts.googleapis.com/css?family=Niramit:300i,400,600&amp;subset=latin-ext,thai,vietnamese" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Lora&amp;subset=vietnamese" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Sorts+Mill+Goudy" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Titillium+Web:300,400,600" rel="stylesheet">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+		<link rel="stylesheet" type="text/css" href="css/Services.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		<script src="js/jquery-1.11.2.min.js"></script>
+
+	</head>
+	<body id="Body">
+
+
+
+
+<header>
+    <div class="container">
+        <div class="header">
+            <div class="header-top">
+                <nav>
+                    <ul>
+                        <li>
+                            <a href="signup.html">Sign Up |</a>
+                        </li>
+                        <li>
+                            <a href="logIn.html">Login |</a>
+                        </li>
+                        <li>
+                            <a href="https://rss.com">RSS Feeds |</a>
+                        </li>
+                        <li>
+                            <a href="ArchivedNews.html">Archived News</a>
+                        </li>
+                    </ul>
+
+                </nav>
+            </div>
+            <div class="header-bottom">
+                <ul>
+                    <li>
+                        <a href="https://www.linkedin.com" class="linkedIn" target="_blank" title="LinkedIn">
+                            <i class="fab fa-linkedin-in"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://twitter.com/" class="twitter" target="_blank" title="Twitter">
+                            <i class="fab fa-twitter"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.pinterest.com/" class="pinterest" target="_blank" title="Pinterest">
+                            <i class="fab fa-pinterest-p"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://plus.google.com" class="googleplus" target="_blank" title="Google Plus">
+                            <i class="fab fa-google-plus-g"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://rss.com" class="rss" target="_blank" title="Rss">
+                            <i class="fas fa-rss"></i>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</header>
