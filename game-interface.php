c2fbb90eb6568c4fac3fb70c3ceae4b1b9f84ae5
PuhizaDoci <puhize.doci@studentet.uni-pr.edu>
2019-06-01 16:22:00 +0200

    game-interface

--- game-interface.php
+++ game-interface.php
@@ -32,7 +32,7 @@
           unset($_COOKIE['my_login']);
           setcookie('my_login', '', time() - 3600, '/');
       ?>
-      <form action="play.php" method="GET"><br>
+      <form action="game.php" method="GET"><br>
           Name: <input type="text" name="login"><br><hr />
       <input type="submit" value="Start Game" id="buttons" >
       </form>
