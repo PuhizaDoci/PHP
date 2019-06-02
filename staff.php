c8ec3d29025c464af45c03a6bbf44a83c2d494e9
Rrezarta Sallauka <rrezarta.sallauka@studentet.uni-pr.edu>
2019-06-02 17:45:09 +0200

    WEB API

--- staff.php
+++ staff.php
@@ -53,7 +53,7 @@ function showUser(str) {
   }
   if (window.XMLHttpRequest) {
     xmlhttp=new XMLHttpRequest();
-  } else { 
+  } else {
     xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
   }
   xmlhttp.onreadystatechange=function() {
@@ -127,6 +127,36 @@ function showUser(str) {
 
     </section>
 </section>
+<?php
+require_once('TwitterAPIExchange.php');
+/** Set access tokens here - see: https://dev.twitter.com/apps/ **/
+$settings = array(
+'oauth_access_token' => "YOUR_OAUTH_ACCESS_TOKEN",
+'oauth_access_token_secret' => "YOUR_OAUTH_ACCESS_TOKEN_SECRET",
+'consumer_key' => "YOUR_CONSUMER_KEY",
+'consumer_secret' => "YOUR_CONSUMER_SECRET"
+);
+$url = "https://api.twitter.com/1.1/statuses/user_timeline.json";
+$requestMethod = "GET";
+if (isset($_GET['user']))  {$user = preg_replace("/[^A-Za-z0-9_]/", '', $_GET['user']);}  else {$user  = "iagdotme";}
+if (isset($_GET['count']) && is_numeric($_GET['count']) {$count = $_GET['count'];} else {$count = 20;}
+$getfield = "?screen_name=$user&count=$count";
+$twitter = new TwitterAPIExchange($settings);
+$string = json_decode($twitter->setGetfield($getfield)
+->buildOauth($url, $requestMethod)
+->performRequest(),$assoc = TRUE);
+if(array_key_exists("errors", $string)) {echo "<h3>Sorry, there was a problem.</h3><p>Twitter returned the following error message:</p><p><em>".$string[errors][0]["message"]."</em></p>";exit();}
+foreach($string as $items)
+    {
+        echo "Time and Date of Tweet: ".$items['created_at']."<br />";
+        echo "Tweet: ". $items['text']."<br />";
+        echo "Tweeted by: ". $items['user']['name']."<br />";
+        echo "Screen name: ". $items['user']['screen_name']."<br />";
+        echo "Followers: ". $items['user']['followers_count']."<br />";
+        echo "Friends: ". $items['user']['friends_count']."<br />";
+        echo "Listed: ". $items['user']['listed_count']."<br /><hr />";
+    }
+?>
 </main>
 <?php include('footer.php');?>
 </body>
