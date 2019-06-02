48bd700bcb903da3cc13acded8a167408bdd9de7
Rrezarta Sallauka <rrezarta.sallauka@studentet.uni-pr.edu>
2019-06-02 14:04:12 +0200

    Create staff.php

---
+++ staff.php
@@ -0,0 +1,47 @@
+<?php include('header.php');?>
+<link rel="stylesheet" type="text/css" href="MainPage.css">
+
+<main>
+    <div class="container">
+        <div class="main-top">
+            <nav>
+                <ul>
+                    <li>
+                        <a href="MainPage.php">
+                            HOME
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="Company.php">COMPANY</a>
+                    </li>
+                    <li>
+                        <a href="Services.php">SERVICES</a>
+                    </li>
+                    <li>
+                        <a href="Staff.php">Staff</a>
+                    </li>
+
+
+        <div class="maintop1">
+            <div class="maintop2">
+                <h1 id="logo" title="Exoy">Exoy</h1>
+                <p><u>Tech Marketplace</u></p>
+            </div>
+        </div>
+    </div>
+    <section class="small-banner partners-banner">
+        <h4>Why us / Customers</h4>
+    <section class="our-costumers text-center section-padding grey-background">
+        <div class="container">
+            <h1 class="main-title">Happy Customers</h1>
+            <p class="description-item">Our Fantastic eXoy Customers</p>
+
+
+        </div>
+    </section>
+
+</main>
+<?php include('footer.php');?>
+</body>
+</html>
